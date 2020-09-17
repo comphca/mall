af6fe4337c0867b44faae42e170f8e8e38979322
comphca <comphca@gmail.com>
2020-09-18 00:35:44 +0800

    获取当前登录用户的信息

--- src/main/java/com/mall/controller/portal/UserController.java
+++ src/main/java/com/mall/controller/portal/UserController.java
@@ -39,9 +39,29 @@ public class UserController {
         return ServerResponse.cerateBySuccess();
     }
 
+    /*注册功能模块请求参数直接封装成一个user对象，这里前台请求参数为username、password、question、answer、email、phone，
+    对于整个bean来说还剩role、创建时间和更新时间三个属性后台会自动设置，不设置就会报错
+
+    时间类型在xml配置文件用函数直接生成
+    */
     @RequestMapping(value = "register",method = RequestMethod.POST)
     @ResponseBody
     public ServerResponse register(User user){
+//        System.out.println(user.getUsername());
+//        System.out.println(user.getRole());
         return iUserService.register(user);
     }
+
+
+    @RequestMapping(value = "get_user_info", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse get_user_info(HttpSession session){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if (user == null){
+            return ServerResponse.createByErrorMessage("没有登录的用户");
+        }
+        return ServerResponse.cerateBySuccess(user);
+    }
+
+
 }
